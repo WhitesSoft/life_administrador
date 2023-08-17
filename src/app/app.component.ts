75c6ad82c90543305da40d292e4f8b526e8a3f75
WhitesSoft <juan.moonteroo@gmail.com>
2023-08-16 21:31:15 -0400

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'life_administrador';
+
+  sidebarOpen = true;
+  darkMode = false;
+
+  menuItems = [
+    { title: "Inicio", iconClass: "bi-house" },
+    { title: "Horarios", iconClass: "bi-calendar-week" },
+    { title: "Historiales", iconClass: "bi-file-medical" },
+    { title: "Pacientes", iconClass: "bi-file-earmark-person" },
+    { title: "Opciones", iconClass: "bi-gear" },
+    { title: "Salir", iconClass: "bi-box-arrow-left" },
+  ]
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+  }
+
 }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "body-color": "var(--body-color)",
+        "sidebar-color": "var(--sidebar-color)",
+        "primary-color": "var(--primary-color)",
+        "primary-color-light": "var(--primary-color-light)",
+        "toggle-color": "var(--toggle-color)",
+        "text-color": "var(--text-color)",
+      },
+    },
+  },
+  plugins: [],
+}
+
